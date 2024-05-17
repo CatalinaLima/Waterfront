a7cdc5192ca125930c58a2efb500bec307e59b66
Catalina Lima Brun <catalinalbrun@gmail.com>
2024-05-17 14:15:43 -0600

    pics vercel

--- src/data/amenities.jsx
+++ src/data/amenities.jsx
@@ -2,7 +2,7 @@ const amenities = [
     {
       id: 1,
       title: "Jacuzzi",
-      icon: '',
+      icon: '/public/assets/jacuzzi_icon.png',
       quantity: 0,
       description: 'Jacuzzi up to 15 people.',
       img: '/public/assets/jacuzzi_img.jpg'
@@ -10,26 +10,26 @@ const amenities = [
     {
       id: 2,
       title: "Sauna",
-      icon: '/public/sauna_icon.png',
+      icon: '/public/assets/sauna_icon.png',
       quantity: 0,
       description: 'Sauna up to 10 people.',
-      img: '/public/sauna_img.jpg'
+      img: '/public/assets/sauna_img.jpg'
     },
     {
       id: 3,
       title: "Yoga studio",
-      icon: '/public/yoga_icon.png',
+      icon: '/public/assets/yoga_icon.png',
       quantity: 0,
       description: 'Yoga up to 40 people from 7am to 11pm.',
-      img: '/public/yoga_img.jpg'
+      img: '/public/assets/yoga_img.jpg'
     },
     {
       id: 4,
       title: "Gym",
-      icon: '/public/gym_icon.png',
+      icon: '/public/assets/gym_icon.png',
       quantity: 0,
       description: 'Gym up to 40 people from 7am to 11pm..',
-      img: '/public/gym_img.jpg'
+      img: '/public/assets/gym_img.jpg'
     },
   ]
 
